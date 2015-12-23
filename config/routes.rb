d0d258d87c0b428488622d789c0a494b5e2aa5bd
Shawn Kearney <shawnpk@ineptsoftware.com>
2015-12-23 08:18:47 -0800

    Change homepage based on logged in user

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :notes
 
+  authenticated :user do
+  	root 'notes#index', as: 'authenticated_root'
+  end
+
   root 'welcome#index'
 end
